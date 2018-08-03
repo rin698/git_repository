ceb39794f515c70be0ee3caf1ca413ad453defd3
rin698 <rin698@live.ru>
2018-08-03 15:23:17 +0300

    Add Spring Boot skeleton

---
+++ src/main/java/com/rin698/git_repository/GitRepositoryApplication.java
@@ -0,0 +1,12 @@
+package com.rin698.git_repository;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitRepositoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitRepositoryApplication.class, args);
+	}
+}
